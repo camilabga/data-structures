17178a031295b6f54d46cbae9fe2f0fea842b4d7
Camila Barbosa <camilagomss@gmail.com>
2019-02-17 12:15:58 -0300

    check_brackets_in_code done

--- check_brackets_in_code/check_brackets.cpp
+++ check_brackets_in_code/check_brackets.cpp
@@ -33,15 +33,43 @@ int main() {
         char next = text[position];
 
         if (next == '(' || next == '[' || next == '{') {
+            opening_brackets_stack.push(Bracket(next, position));
             // Process opening bracket, write your code here
         }
 
         if (next == ')' || next == ']' || next == '}') {
+            if (!opening_brackets_stack.empty()) {
+                if(!opening_brackets_stack.top().Matchc(next)){
+                    cout << position+1 << endl;
+                    return 0;
+                } else {
+                    opening_brackets_stack.pop();
+                }
+            } else {
+                cout << position+1 << endl;
+                return 0;
+            }
+                
             // Process closing bracket, write your code here
         }
+    }
+
+    Bracket b('a', 1);
+
+    if (opening_brackets_stack.empty()) {
+        cout << "Success" << endl;
+    } else {
+        while (!opening_brackets_stack.empty()) {
+            b = opening_brackets_stack.top();
+            opening_brackets_stack.pop();
+        }
+
+        cout << b.position+1 << endl;
     }
 
     // Printing answer, write your code here
 
     return 0;
 }
+
+//spOletO!172723
